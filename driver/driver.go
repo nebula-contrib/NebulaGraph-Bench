3e63b661d2c1ce61fb00e2480a55d429fdccd5ee
Simon Liu <331435+monadbobo@users.noreply.github.com>
2020-07-23 11:03:42 +0800

    1 Fixed the import path of packages 2 Re-formatting code 3 Added CI(github workflow)

--- driver/driver.go
+++ driver/driver.go
@@ -1,156 +1,157 @@
 package driver
 
 import (
-    "log"
-    "fmt"
-    "time"
-    "context"
-    "sync"
-    "errors"
-    "runtime"
-    "database/sql"
-    "golang.org/x/time/rate"
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/dutor/nebula-bench/util"
-    nebula "github.com/vesoft-inc/nebula-go"
-    graph "github.com/vesoft-inc/nebula-go/nebula/graph"
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"runtime"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/vesoft-inc/nebula-bench/util"
+	nebula "github.com/vesoft-inc/nebula-go"
+	graph "github.com/vesoft-inc/nebula-go/nebula/graph"
+	"golang.org/x/time/rate"
 )
 
 type Driver struct {
-    counter         uint64
-    lock            sync.Mutex
-    config         *util.BenchConfig
-    limiters     []*rate.Limiter
-    clients      []*nebula.GraphClient
-    cancellers    []context.CancelFunc
-    workload        util.QueryWorkload
-    sstats         *util.Stats
-    cstats         *util.Stats
-    wg             *sync.WaitGroup
-    db             *sql.DB
-    test_id         int64
-    done            chan int
+	counter    uint64
+	lock       sync.Mutex
+	config     *util.BenchConfig
+	limiters   []*rate.Limiter
+	clients    []*nebula.GraphClient
+	cancellers []context.CancelFunc
+	workload   util.QueryWorkload
+	sstats     *util.Stats
+	cstats     *util.Stats
+	wg         *sync.WaitGroup
+	db         *sql.DB
+	test_id    int64
+	done       chan int
 }
 
 func NewDriver(cfg *util.BenchConfig) (*Driver, error) {
-    if cfg == nil {
-        return nil, errors.New("BenchConfig is nil")
-    }
-    if len(cfg.GraphDaemons) == 0 {
-        return nil, errors.New("graph-daemons must be specified")
-    }
-    if cfg.Concurrent <= 0 {
-        cfg.Concurrent = runtime.NumCPU()
-        log.Printf("Use number of CPU as `concurrent': %v", cfg.Concurrent)
-    }
+	if cfg == nil {
+		return nil, errors.New("BenchConfig is nil")
+	}
+	if len(cfg.GraphDaemons) == 0 {
+		return nil, errors.New("graph-daemons must be specified")
+	}
+	if cfg.Concurrent <= 0 {
+		cfg.Concurrent = runtime.NumCPU()
+		log.Printf("Use number of CPU as `concurrent': %v", cfg.Concurrent)
+	}
 
-    driver := &Driver{}
-    for i := 0; i < cfg.Concurrent; i++ {
-        if c,e := nebula.NewClient(cfg.GraphDaemons[0].String(), nebula.WithTimeout(0)); e != nil {
-            return nil, e
-        } else if e = c.Connect(cfg.User, cfg.Pass); e != nil {
-            return nil, e
-        } else if _,e := c.Execute("USE " + cfg.Space); e != nil {
-            return nil, e
-        } else {
-            driver.clients = append(driver.clients, c)
-        }
-    }
+	driver := &Driver{}
+	for i := 0; i < cfg.Concurrent; i++ {
+		if c, e := nebula.NewClient(cfg.GraphDaemons[0].String(), nebula.WithTimeout(0)); e != nil {
+			return nil, e
+		} else if e = c.Connect(cfg.User, cfg.Pass); e != nil {
+			return nil, e
+		} else if _, e := c.Execute("USE " + cfg.Space); e != nil {
+			return nil, e
+		} else {
+			driver.clients = append(driver.clients, c)
+		}
+	}
 
-    for i := 0; i < cfg.Concurrent; i++ {
-        limiter := rate.NewLimiter(rate.Limit(cfg.Rate / cfg.Concurrent), 16)
-        driver.limiters = append(driver.limiters, limiter)
-    }
+	for i := 0; i < cfg.Concurrent; i++ {
+		limiter := rate.NewLimiter(rate.Limit(cfg.Rate/cfg.Concurrent), 16)
+		driver.limiters = append(driver.limiters, limiter)
+	}
 
-    if p,e := util.NewWorkload(&cfg.Workload); e != nil {
-        return nil,e
-    } else {
-        driver.workload = p
-    }
+	if p, e := util.NewWorkload(&cfg.Workload); e != nil {
+		return nil, e
+	} else {
+		driver.workload = p
+	}
 
-    for ; cfg.MysqlDSN != ""; {
-        if db,e := sql.Open("mysql", cfg.MysqlDSN); e != nil {
-            return nil, e
-        } else if e := db.Ping(); e != nil {
-            log.Println(e)
-            break
-        } else if res,e := db.Exec(`INSERT INTO tests(name) VALUES(?)`, cfg.Name); e != nil {
-            log.Println(e)
-            break
-        } else {
-            driver.db = db
-            driver.test_id,_ = res.LastInsertId()
-        }
-        break
-    }
+	for cfg.MysqlDSN != "" {
+		if db, e := sql.Open("mysql", cfg.MysqlDSN); e != nil {
+			return nil, e
+		} else if e := db.Ping(); e != nil {
+			log.Println(e)
+			break
+		} else if res, e := db.Exec(`INSERT INTO tests(name) VALUES(?)`, cfg.Name); e != nil {
+			log.Println(e)
+			break
+		} else {
+			driver.db = db
+			driver.test_id, _ = res.LastInsertId()
+		}
+		break
+	}
 
-    driver.config = cfg
-    driver.sstats = util.NewStats("server-side")
-    driver.cstats = util.NewStats("client-side")
+	driver.config = cfg
+	driver.sstats = util.NewStats("server-side")
+	driver.cstats = util.NewStats("client-side")
 
-    return driver, nil
+	return driver, nil
 }
 
 func (this *Driver) send(idx int, ctx context.Context) {
-    defer this.wg.Done()
-    c := this.workload.Stream()
+	defer this.wg.Done()
+	c := this.workload.Stream()
 Loop:
-    for {
-        select {
-        case <-ctx.Done():
-            break Loop
-        default:
-            this.limiters[idx].Wait(ctx)
-            s, ok := <-c
-            if !ok {
-                break Loop
-            }
-            start := time.Now()
-            if resp,e := this.clients[idx].Execute(s); e != nil {
-                log.Println(e)
-            } else if resp.GetErrorCode() != graph.ErrorCode_SUCCEEDED {
-                log.Printf("Statement: %s, ErrorCode: %v, ErrorMsg: %s",
-                           s, resp.GetErrorCode(), resp.GetErrorMsg())
-            } else {
-                this.sstats.Add(int(resp.LatencyInUs))
-                this.cstats.Add(int(time.Since(start).Microseconds()))
-            }
-        }
-    }
+	for {
+		select {
+		case <-ctx.Done():
+			break Loop
+		default:
+			this.limiters[idx].Wait(ctx)
+			s, ok := <-c
+			if !ok {
+				break Loop
+			}
+			start := time.Now()
+			if resp, e := this.clients[idx].Execute(s); e != nil {
+				log.Println(e)
+			} else if resp.GetErrorCode() != graph.ErrorCode_SUCCEEDED {
+				log.Printf("Statement: %s, ErrorCode: %v, ErrorMsg: %s",
+					s, resp.GetErrorCode(), resp.GetErrorMsg())
+			} else {
+				this.sstats.Add(int(resp.LatencyInUs))
+				this.cstats.Add(int(time.Since(start).Microseconds()))
+			}
+		}
+	}
 }
 
 func (this *Driver) Start() {
-    n := this.config.Concurrent
-    this.wg = &sync.WaitGroup{}
-    this.wg.Add(n)
-    this.done = make(chan int)
-    for i := 0; i < n; i++ {
-        ctx,canceller := context.WithCancel(context.Background())
-        this.cancellers = append(this.cancellers, canceller)
-        go this.send(i, ctx)
-    }
-    go func() {
-        this.wg.Wait()
-        this.sstats.Done()
-        this.cstats.Done()
-        if this.db != nil {
-            this.sstats.WriteToDB(this.db, this.test_id)
-            this.cstats.WriteToDB(this.db, this.test_id)
-        }
-        this.sstats.WriteTrendsToCSV("server-side-qps-latency-trends.csv")
-        this.sstats.WriteHistToCSV("server-side-latency-hist.csv")
-        fmt.Println(this.sstats.OverallMetric())
-        fmt.Println(this.cstats.OverallMetric())
-        this.done <- 0
-    }()
+	n := this.config.Concurrent
+	this.wg = &sync.WaitGroup{}
+	this.wg.Add(n)
+	this.done = make(chan int)
+	for i := 0; i < n; i++ {
+		ctx, canceller := context.WithCancel(context.Background())
+		this.cancellers = append(this.cancellers, canceller)
+		go this.send(i, ctx)
+	}
+	go func() {
+		this.wg.Wait()
+		this.sstats.Done()
+		this.cstats.Done()
+		if this.db != nil {
+			this.sstats.WriteToDB(this.db, this.test_id)
+			this.cstats.WriteToDB(this.db, this.test_id)
+		}
+		this.sstats.WriteTrendsToCSV("server-side-qps-latency-trends.csv")
+		this.sstats.WriteHistToCSV("server-side-latency-hist.csv")
+		fmt.Println(this.sstats.OverallMetric())
+		fmt.Println(this.cstats.OverallMetric())
+		this.done <- 0
+	}()
 }
 
 func (this *Driver) Stop() {
-    for _, canceller := range this.cancellers {
-        canceller()
-    }
+	for _, canceller := range this.cancellers {
+		canceller()
+	}
 }
 
 func (this *Driver) Done() <-chan int {
-    return this.done
+	return this.done
 }
--- util/stats.go
+++ util/stats.go
@@ -1,241 +1,241 @@
 package util
 
 import (
-    "time"
-    "log"
-    "fmt"
-    "sync"
-    "database/sql"
-    "strconv"
-    "os"
-    "encoding/csv"
+	"database/sql"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	"time"
 )
 
 const (
-    kMaxLatency         = 1000000000        // Microseconds
-    kBucketWidth        = 100               // Microseconds
-    kBucketNum          = kMaxLatency/kBucketWidth + 1
-    kInterval           = 10               // Seconds
+	kMaxLatency  = 1000000000 // Microseconds
+	kBucketWidth = 100        // Microseconds
+	kBucketNum   = kMaxLatency/kBucketWidth + 1
+	kInterval    = 10 // Seconds
 )
 
 type Metric struct {
-    id                  int64
-    ts                  int64
-    samples             int64
-    qps                 int64
-    avg                 int64
-    p999                int64
-    p99                 int64
-    p95                 int64
+	id      int64
+	ts      int64
+	samples int64
+	qps     int64
+	avg     int64
+	p999    int64
+	p99     int64
+	p95     int64
 }
 
 type Histogram struct {
-    buckets             []uint32
-    num                 int64
-    start               time.Time
+	buckets []uint32
+	num     int64
+	start   time.Time
 }
 
 type Stats struct {
-    name                string
-    done                chan bool
-    ticker             *time.Ticker
-    current            *Histogram
-    overall            *Histogram
-    trends              []*Metric
-    lock                sync.Mutex
-    lastid              int64
+	name    string
+	done    chan bool
+	ticker  *time.Ticker
+	current *Histogram
+	overall *Histogram
+	trends  []*Metric
+	lock    sync.Mutex
+	lastid  int64
 }
 
 func NewStats(name string) *Stats {
-    stats := &Stats{}
-    stats.current = newHist()
-    stats.overall = newHist()
-    stats.name = name
-    stats.done = make(chan bool)
-    stats.lastid = 0
-    stats.ticker = time.NewTicker(kInterval * time.Second)
-    go stats.tick()
-    return stats
+	stats := &Stats{}
+	stats.current = newHist()
+	stats.overall = newHist()
+	stats.name = name
+	stats.done = make(chan bool)
+	stats.lastid = 0
+	stats.ticker = time.NewTicker(kInterval * time.Second)
+	go stats.tick()
+	return stats
 }
 
 func newHist() *Histogram {
-    hist := &Histogram{}
-    hist.buckets = make([]uint32, kBucketNum)
-    hist.start = time.Now()
-    hist.num = 0
-    return hist
+	hist := &Histogram{}
+	hist.buckets = make([]uint32, kBucketNum)
+	hist.start = time.Now()
+	hist.num = 0
+	return hist
 }
 
 func (this *Stats) Add(us int) {
-    idx := us / kBucketWidth;
-    if idx >= kBucketNum {
-        idx = kBucketNum - 1
-    }
-    this.lock.Lock()
-    this.current.buckets[idx]++
-    this.current.num++
-    this.overall.buckets[idx]++
-    this.overall.num++
-    this.lock.Unlock()
+	idx := us / kBucketWidth
+	if idx >= kBucketNum {
+		idx = kBucketNum - 1
+	}
+	this.lock.Lock()
+	this.current.buckets[idx]++
+	this.current.num++
+	this.overall.buckets[idx]++
+	this.overall.num++
+	this.lock.Unlock()
 }
 
 func (this *Stats) collect(hist *Histogram) *Metric {
-    metric := &Metric{}
-    if hist.num == 0 {
-        return metric
-    }
-    duration := time.Since(hist.start)
-    ms := duration.Milliseconds()
-    // Ignore this interval if too short
-    if ms == 0 {
-        return metric
-    }
-    metric.qps = hist.num * 1000 / ms
-    metric.id = this.lastid
-    this.lastid++
-    metric.samples = hist.num
-    metric.ts = time.Now().Unix()
-    var total_us int64 = 0
-    var p999_limit int64 = hist.num / 1000
-    var p999_count int64 = 0
-    var p999 int64 = 0
-    var p99_limit int64 = hist.num / 100
-    var p99_count int64 = 0
-    var p99 int64 = 0
-    var p95_limit int64 = hist.num * 5 / 100
-    var p95_count int64 = 0
-    var p95 int64 = 0
-    for i := kBucketNum - 1; i >= 0; i-- {
-        total_us += kBucketWidth * int64(hist.buckets[i]) * int64(i)
-        if p999_count < p999_limit {
-            p999_count += int64(hist.buckets[i])
-            p999 = int64(i * kBucketWidth)
-        }
-        if p99_count < p99_limit {
-            p99_count += int64(hist.buckets[i])
-            p99 = int64(i * kBucketWidth)
-        }
-        if p95_count < p95_limit {
-            p95_count += int64(hist.buckets[i])
-            p95 = int64(i * kBucketWidth)
-        }
-    }
-    metric.avg = total_us / hist.num
-    metric.p999 = p999
-    metric.p99 = p99
-    metric.p95 = p95
-    return metric
+	metric := &Metric{}
+	if hist.num == 0 {
+		return metric
+	}
+	duration := time.Since(hist.start)
+	ms := duration.Milliseconds()
+	// Ignore this interval if too short
+	if ms == 0 {
+		return metric
+	}
+	metric.qps = hist.num * 1000 / ms
+	metric.id = this.lastid
+	this.lastid++
+	metric.samples = hist.num
+	metric.ts = time.Now().Unix()
+	var total_us int64 = 0
+	var p999_limit int64 = hist.num / 1000
+	var p999_count int64 = 0
+	var p999 int64 = 0
+	var p99_limit int64 = hist.num / 100
+	var p99_count int64 = 0
+	var p99 int64 = 0
+	var p95_limit int64 = hist.num * 5 / 100
+	var p95_count int64 = 0
+	var p95 int64 = 0
+	for i := kBucketNum - 1; i >= 0; i-- {
+		total_us += kBucketWidth * int64(hist.buckets[i]) * int64(i)
+		if p999_count < p999_limit {
+			p999_count += int64(hist.buckets[i])
+			p999 = int64(i * kBucketWidth)
+		}
+		if p99_count < p99_limit {
+			p99_count += int64(hist.buckets[i])
+			p99 = int64(i * kBucketWidth)
+		}
+		if p95_count < p95_limit {
+			p95_count += int64(hist.buckets[i])
+			p95 = int64(i * kBucketWidth)
+		}
+	}
+	metric.avg = total_us / hist.num
+	metric.p999 = p999
+	metric.p99 = p99
+	metric.p95 = p95
+	return metric
 }
 
 func (this *Stats) tick() {
-    var metric *Metric
+	var metric *Metric
 Loop:
-    for {
-        select {
-        case <-this.done:
-            metric = this.collect(this.current)
-            this.trends = append(this.trends, metric)
-            log.Printf("[%s] %+v\n", this.name, *metric)
-            break Loop
-        case <-this.ticker.C:
-            this.lock.Lock()
-            cur := this.current
-            this.current = newHist()
-            this.lock.Unlock()
-            metric = this.collect(cur)
-            this.trends = append(this.trends, metric)
-            log.Printf("[%s] %+v\n", this.name, *metric)
-        }
-    }
-    this.done <- true
+	for {
+		select {
+		case <-this.done:
+			metric = this.collect(this.current)
+			this.trends = append(this.trends, metric)
+			log.Printf("[%s] %+v\n", this.name, *metric)
+			break Loop
+		case <-this.ticker.C:
+			this.lock.Lock()
+			cur := this.current
+			this.current = newHist()
+			this.lock.Unlock()
+			metric = this.collect(cur)
+			this.trends = append(this.trends, metric)
+			log.Printf("[%s] %+v\n", this.name, *metric)
+		}
+	}
+	this.done <- true
 }
 
 func (this *Stats) Done() {
-    this.ticker.Stop()
-    this.done <- true
-    <-this.done
+	this.ticker.Stop()
+	this.done <- true
+	<-this.done
 }
 
 func (this *Stats) PlotTrends(file string) error {
-    return nil
+	return nil
 }
 
 func (this *Stats) WriteTrendsToCSV(file string) error {
-    var writer *csv.Writer
-    if f,e := os.Create(file); e != nil {
-        return e
-    } else {
-        defer f.Close()
-        writer = csv.NewWriter(f)
-        defer writer.Flush()
-    }
-    writer.Comma = []rune("\t")[0]
+	var writer *csv.Writer
+	if f, e := os.Create(file); e != nil {
+		return e
+	} else {
+		defer f.Close()
+		writer = csv.NewWriter(f)
+		defer writer.Flush()
+	}
+	writer.Comma = []rune("\t")[0]
 
-    var rows [][]string
-    rows = append(rows, []string{"id", "ts", "qps", "avg", "P99", "p95", "P999", "samples"})
-    for _,m := range this.trends {
-        var row []string
-        row = append(row, strconv.FormatInt(m.id, 10))
-        row = append(row, strconv.FormatInt(m.ts, 10))
-        row = append(row, strconv.FormatInt(m.qps, 10))
-        row = append(row, strconv.FormatInt(m.avg, 10))
-        row = append(row, strconv.FormatInt(m.p99, 10))
-        row = append(row, strconv.FormatInt(m.p95, 10))
-        row = append(row, strconv.FormatInt(m.p999, 10))
-        row = append(row, strconv.FormatInt(m.samples, 10))
-        rows = append(rows, row)
-    }
-    writer.WriteAll(rows)
+	var rows [][]string
+	rows = append(rows, []string{"id", "ts", "qps", "avg", "P99", "p95", "P999", "samples"})
+	for _, m := range this.trends {
+		var row []string
+		row = append(row, strconv.FormatInt(m.id, 10))
+		row = append(row, strconv.FormatInt(m.ts, 10))
+		row = append(row, strconv.FormatInt(m.qps, 10))
+		row = append(row, strconv.FormatInt(m.avg, 10))
+		row = append(row, strconv.FormatInt(m.p99, 10))
+		row = append(row, strconv.FormatInt(m.p95, 10))
+		row = append(row, strconv.FormatInt(m.p999, 10))
+		row = append(row, strconv.FormatInt(m.samples, 10))
+		rows = append(rows, row)
+	}
+	writer.WriteAll(rows)
 
-    return nil
+	return nil
 }
 
 func (this *Stats) WriteHistToCSV(file string) error {
-    var writer *csv.Writer
-    if f,e := os.Create(file); e != nil {
-        return e
-    } else {
-        defer f.Close()
-        writer = csv.NewWriter(f)
-        defer writer.Flush()
-    }
-    writer.Comma = []rune("\t")[0]
+	var writer *csv.Writer
+	if f, e := os.Create(file); e != nil {
+		return e
+	} else {
+		defer f.Close()
+		writer = csv.NewWriter(f)
+		defer writer.Flush()
+	}
+	writer.Comma = []rune("\t")[0]
 
-    var rows [][]string
-    rows = append(rows, []string{"ms", "samples"})
-    var maxBucket int = kBucketNum
-    for i := kBucketNum - 1; i > 0; i-- {
-        if this.overall.buckets[i] != 0 {
-            maxBucket = i
-            break
-        }
-    }
-    step := 1000 / kBucketWidth;
-    for i := 0; i < maxBucket; i += step {
-        var row []string
-        ms := int64(i / step)
-        var sum int64 = 0
-        for j := i; j < i + step; j++ {
-            sum += int64(this.overall.buckets[j])
-        }
-        row = append(row, strconv.FormatInt(ms, 10))
-        row = append(row, strconv.FormatInt(sum, 10))
-        rows = append(rows, row)
-    }
-    writer.WriteAll(rows)
+	var rows [][]string
+	rows = append(rows, []string{"ms", "samples"})
+	var maxBucket int = kBucketNum
+	for i := kBucketNum - 1; i > 0; i-- {
+		if this.overall.buckets[i] != 0 {
+			maxBucket = i
+			break
+		}
+	}
+	step := 1000 / kBucketWidth
+	for i := 0; i < maxBucket; i += step {
+		var row []string
+		ms := int64(i / step)
+		var sum int64 = 0
+		for j := i; j < i+step; j++ {
+			sum += int64(this.overall.buckets[j])
+		}
+		row = append(row, strconv.FormatInt(ms, 10))
+		row = append(row, strconv.FormatInt(sum, 10))
+		rows = append(rows, row)
+	}
+	writer.WriteAll(rows)
 
-    return nil
+	return nil
 }
 
 func (this *Stats) OverallMetric() string {
-    return fmt.Sprintf("[%s][overall] %+v", this.name, *this.collect(this.overall))
+	return fmt.Sprintf("[%s][overall] %+v", this.name, *this.collect(this.overall))
 }
 
 func (this *Stats) WriteToDB(db *sql.DB, test_id int64) {
-    for _,m := range this.trends {
-        if _,e := db.Exec(`INSERT INTO latency(type, timestamp,samples,qps,average,p95,p99,p999,test)
+	for _, m := range this.trends {
+		if _, e := db.Exec(`INSERT INTO latency(type, timestamp,samples,qps,average,p95,p99,p999,test)
         VALUES(?,?,?,?,?,?,?,?,?)`, this.name, m.ts, m.samples, m.qps, m.avg, m.p95, m.p99, m.p999, test_id); e != nil {
-            log.Fatal(e)
-        }
-    }
+			log.Fatal(e)
+		}
+	}
 }
