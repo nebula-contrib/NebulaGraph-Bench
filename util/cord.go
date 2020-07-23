a2551c59176ffcd2265eaa6e66a378894b512ea7
Simon Liu <331435+monadbobo@users.noreply.github.com>
2020-07-23 12:19:24 +0800

    Fixed the import path of packages (#1)
    1 Fixed the import path of packages
    2 Re-formatting code
    3 Added CI(github workflow)

--- util/cord.go
+++ util/cord.go
@@ -1,57 +1,57 @@
 package util
 
 type Cord struct {
-    b           []byte
+	b []byte
 }
 
 func (this *Cord) Len() int {
-    return len(this.b)
+	return len(this.b)
 }
 
 func (this *Cord) Empty() bool {
-    return len(this.b) == 0
+	return len(this.b) == 0
 }
 
 func (this *Cord) Cap() int {
-    return cap(this.b)
+	return cap(this.b)
 }
 
 func (this *Cord) Clear() {
-    this.b = this.b[0:0]
+	this.b = this.b[0:0]
 }
 
 func (this *Cord) Write(s []byte) {
-    prev,delta := len(this.b),len(s)
-    this.Grow(delta)
-    this.b = this.b[:prev+delta]
-    copy(this.b[prev:], s)
+	prev, delta := len(this.b), len(s)
+	this.Grow(delta)
+	this.b = this.b[:prev+delta]
+	copy(this.b[prev:], s)
 }
 
 func (this *Cord) WriteString(s string) {
-    this.Grow(len(s))
-    prev,delta := len(this.b),len(s)
-    this.Grow(delta)
-    this.b = this.b[:prev+delta]
-    copy(this.b[prev:], s)
+	this.Grow(len(s))
+	prev, delta := len(this.b), len(s)
+	this.Grow(delta)
+	this.b = this.b[:prev+delta]
+	copy(this.b[prev:], s)
 }
 
 func (this *Cord) Grow(delta int) {
-    left := cap(this.b) - len(this.b)
-    if left >= delta {
-        return
-    }
+	left := cap(this.b) - len(this.b)
+	if left >= delta {
+		return
+	}
 
-    grown := len(this.b) + delta * 4
-    bb := make([]byte, grown)
-    bb = bb[:len(this.b)]
-    copy(bb, this.b)
-    this.b = bb
+	grown := len(this.b) + delta*4
+	bb := make([]byte, grown)
+	bb = bb[:len(this.b)]
+	copy(bb, this.b)
+	this.b = bb
 }
 
 func (this *Cord) DropEnd(n int) {
-    this.b = this.b[0:len(this.b)-n]
+	this.b = this.b[0 : len(this.b)-n]
 }
 
 func (this *Cord) String() string {
-    return string(this.b)
+	return string(this.b)
 }
