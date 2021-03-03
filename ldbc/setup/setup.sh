f7082b84bf6f94195e73b2586988b1c156c0224b
tom-chensf <73873134+tom-chensf@users.noreply.github.com>
2021-03-03 23:00:31 +0800

    nebula bench2.0

--- ldbc/setup/setup.sh
+++ ldbc/setup/setup.sh
@@ -6,7 +6,7 @@ if [ $# -lt 1 ] ; then
     echo "Please input jmeter install path"
     exit
 else
-    path=$1
+    jmeter_install_path=$1
     echo "jmeter install path is $1"
 fi
 
@@ -20,12 +20,21 @@ fi
  
 cd -
 
-wget -P $path https://mirrors.bfsu.edu.cn/apache//jmeter/binaries/apache-jmeter-5.4.zip
-unzip $path/apache-jmeter-5.4.zip  -d $path/
+wget -P $jmeter_install_path https://mirrors.bfsu.edu.cn/apache/jmeter/binaries/apache-jmeter-5.4.zip
+unzip ${jmeter_install_path}/apache-jmeter-5.4.zip  -d $jmeter_install_path
 if [ $? != 0 ] ; then
    echo "install jmeter failed!"
    exit
 fi
+
+jar_path=${java_prj_path}/target/LdbcGoStep-2-jar-with-dependencies.jar
+jmeter_lib_path=${jmeter_install_path}/apache-jmeter-5.4/lib/ext
+cp -rf ${jar_path} ${jmeter_lib_path}/LdbcGoStep-2-jar-with-dependencies.jar
+if [ $? != 0 ] ; then
+    echo "cp -rf ${jar_path} ${jmeter_lib_path} failed"
+    exit
+fi
+
 
 pip3 install --user -r  ${curr_path}/requirements.txt  -i https://mirrors.aliyun.com/pypi/simple/
 
--- run.sh
+++ run.sh
@@ -1,21 +1,10 @@
 #! /usr/bin/env bash
-
 curr_path=$(readlink -f "$(dirname "$0")")
-java_prj_path=${curr_path}/util/LdbcGoStep
 jmeter_path=$1
-jmeter_lib_path=${jmeter_path}/lib/ext
 datetime=`date +%Y%m%d_%H%M%S_%N |cut -b1-20`
 test_path=${jmeter_path}/${datetime}
-
-
-jar_path=${java_prj_path}/target/LdbcGoStep-2-jar-with-dependencies.jar
-cp -rf ${jar_path} ${jmeter_lib_path}/LdbcGoStep-2-jar-with-dependencies.jar
-if [ $? != 0 ] ; then
-    echo "cp -rf ${jar_path} ${jmeter_lib_path} failed"
-    exit
-fi
-
 jmx_path=${curr_path}/ldbc/jmx/go_step.jmx
+
 mkdir ${test_path} &&  cp -rf ${jmx_path} ${test_path}/go_step.jmx
 
 if [ $? != 0 ] ; then
@@ -26,6 +15,7 @@ fi
 perftest_cmd1="${jmeter_path}/bin/jmeter.sh -n -t ${test_path}/go_step.jmx -l ${test_path}/go_step.jtl -j ${test_path}/go_step.log"
 perftest_cmd2="${jmeter_path}/bin/jmeter.sh -g ${test_path}/go_step.jtl -o ${test_path}/go_step"
 perftest_cmd3="python3 ${curr_path}/ldbc/scripts/statistics.py ${test_path}/go_step.jtl"
+
 $perftest_cmd1
 if [ $? != 0 ] ; then
     echo "$perftest_cmd1 failed!"
