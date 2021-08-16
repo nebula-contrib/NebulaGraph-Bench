8debf90dc4ddc84b4617e99aea75719e4a5a74c9
Harris.Chu <1726587+HarrisChu@users.noreply.github.com>
2021-08-16 10:06:21 +0800

    Enhance (#35)
    * enhance bench, can use mote k6 options
    
    * fix typo
    
    * fix github action

--- nebula_bench/stress.py
+++ nebula_bench/stress.py
@@ -22,19 +22,11 @@ def load_scenarios(scenarios):
 
 
 class Stress(object):
+    DEFAULT_VU = 100
+    DEFAULT_DURATION = "60s"
+
     def __init__(
-        self,
-        folder,
-        address,
-        user,
-        password,
-        space,
-        vid_type,
-        scenarios,
-        vu,
-        duration,
-        dry_run,
-        **kwargs
+        self, folder, address, user, password, space, vid_type, scenarios, args, dry_run, **kwargs
     ):
         self.folder = folder or setting.DATA_FOLDER
         self.address = address or setting.NEBULA_ADDRESS
@@ -44,9 +36,8 @@ class Stress(object):
         self.vid_type = vid_type
         self.scenarios = []
         self.output_folder = "output"
-        self.vu = vu
-        self.duration = duration
         self.dry_run = dry_run
+        self.args = args
         self.scenarios = load_scenarios(scenarios)
         logger.info("total stress test scenarios is {}".format(len(self.scenarios)))
 
@@ -72,8 +63,7 @@ class StressFactory(object):
         space,
         vid_type,
         scenarios,
-        vu,
-        duration,
+        args,
         dry_run=None,
         **kwargs
     ):
@@ -81,18 +71,10 @@ class StressFactory(object):
             raise Exception("not impletment this test tool, tool is {}".format(_type))
 
         clazz = cls.get_all_stress_class().get("{}Stress".format(_type.upper()), None)
+        if args is not None:
+            args = args.strip()
         return clazz(
-            folder,
-            address,
-            user,
-            password,
-            space,
-            vid_type,
-            scenarios,
-            vu,
-            duration,
-            dry_run,
-            **kwargs
+            folder, address, user, password, space, vid_type, scenarios, args, dry_run, **kwargs
         )
 
     @classmethod
@@ -140,35 +122,99 @@ class K6Stress(Stress):
         )
         jinja_dump(template_file, "{}/{}.js".format(self.output_folder, name), kwargs)
 
+    def _get_params(self):
+        """
+        e.g.
+        args:
+            "-s 60s:0 -s 40s:30 -v"
+        return:
+            {
+                "-s": ["60s:0", "40s:30"],
+                "-v": None
+            }
+        """
+        r = {}
+        if self.args is None:
+            return r
+
+        key, value = None, None
+        for item in self.args.split(" "):
+            if item.startswith("-"):
+                if key is not None and key not in r:
+                    r[key] = None
+                key = item
+            elif item.strip() != "":
+                value = item
+                if key not in r:
+                    r[key] = [value]
+                else:
+                    r[key].append(value)
+
+        if key is not None and key not in r:
+            r[key] = None
+        return r
+
     def run(self):
         logger.info("run stress test in k6")
-        logger.info(
-            "every scenario would run by {} vus and last {} secconds".format(self.vu, self.duration)
-        )
-        Path(self.output_folder).mkdir(exist_ok=True)
-        for scenario in self.scenarios:
+        params = self._get_params()
 
+        # cannot use both stage and vu
+        run_with_stage = "-s" in params or "--stage" in params
+        vu = self.DEFAULT_VU
+        duration = self.DEFAULT_DURATION
+        if "-u" in params:
+            vu = params.pop("-u")[0]
+        if "--vus" in params:
+            vu = params.pop("--vus")[0]
+        if "-vu" in params:
+            vu = params.pop("-vu")[0]
+
+        if "-d" in params:
+            duration = params.pop("-d")[0]
+        if "--duration" in params:
+            duration = params.pop("--duration")[0]
+
+        logger.info("every scenario would run by {} vus and last {}".format(vu, duration))
+        Path(self.output_folder).mkdir(exist_ok=True)
+        if "--summary-trend-stats" not in params:
+            params["--summary-trend-stats"] = ["min,avg,med,max,p(90),p(95),p(99)"]
+        if setting.INFLUXDB_URL is not None and "--out" not in params and "-o" not in params:
+            params["--out"] = ["influxdb={}".format(setting.INFLUXDB_URL)]
+
+        for scenario in self.scenarios:
             self.dump_config(scenario)
-            # run k6
-            command = [
-                "scripts/k6",
-                "run",
-                "{}/{}.js".format(self.output_folder, scenario.name),
-                "-u",
-                str(self.vu),
-                "-d",
-                "{}s".format(self.duration),
-                "--summary-trend-stats",
-                "min,avg,med,max,p(90),p(95),p(99)",
-                "--summary-export",
-                "{}/result_{}.json".format(self.output_folder, scenario.name),
-            ]
-            if setting.INFLUXDB_URL is not None:
-                command.append("--out")
-                command.append("influxdb={}".format(setting.INFLUXDB_URL))
+            if run_with_stage:
+                command = [
+                    "scripts/k6",
+                    "run",
+                    "{}/{}.js".format(self.output_folder, scenario.name),
+                ]
+            else:
+                command = [
+                    "scripts/k6",
+                    "run",
+                    "{}/{}.js".format(self.output_folder, scenario.name),
+                    "-u",
+                    str(vu),
+                    "-d",
+                    str(duration),
+                ]
+
+            if "--summary-export" not in params:
+                params["--summary-export"] = [
+                    "{}/result_{}.json".format(self.output_folder, scenario.name)
+                ]
+
+            for param, values in params.items():
+                if values is None:
+                    command.append(param)
+                else:
+                    for v in values:
+                        command.append(param)
+                        command.append(v)
 
             click.echo("run command as below:")
-            click.echo(" ".join(command))
+            click.echo(" ".join([x if "(" not in x else '"{}"'.format(x) for x in command]))
             if self.dry_run is not None and self.dry_run:
                 continue
             run_process(command)
