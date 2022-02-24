b271f9ad1f0aa304022132c4471bb284345b05b9
HarrisChu <1726587+HarrisChu@users.noreply.github.com>
2022-02-24 16:25:26 +0800

    fix yield

--- nebula_bench/scenarios/go.py
+++ nebula_bench/scenarios/go.py
@@ -4,21 +4,21 @@ from nebula_bench.common.base import BaseScenario
 
 class BaseGoScenario(BaseScenario):
     abstract = True
-    nGQL = "GO 1 STEP FROM {} OVER KNOWS"
+    nGQL = "GO 1 STEP FROM {} OVER KNOWS yield KNOWS.creationDate"
     csv_path = "social_network/dynamic/person.csv"
     csv_index = [0]
 
 
 class Go1Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 1 STEP FROM {} OVER KNOWS"
+    nGQL = "GO 1 STEP FROM {} OVER KNOWS yield KNOWS.creationDate"
 
 
 class Go2Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 2 STEP FROM {} OVER KNOWS"
+    nGQL = "GO 2 STEP FROM {} OVER KNOWS yield KNOWS.creationDate"
 
 
 class Go3Step(BaseGoScenario):
     abstract = False
-    nGQL = "GO 3 STEP FROM {} OVER KNOWS"
+    nGQL = "GO 3 STEP FROM {} OVER KNOWS yield KNOWS.creationDate"
