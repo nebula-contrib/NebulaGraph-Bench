7c03cd3ed2e5e9f51a68c18de33dbdfc8b7a9ad7
Hao Wen <19355821+wenhaocs@users.noreply.github.com>
2022-04-19 10:20:13 +0800

    yield something find path statment

--- nebula_bench/scenarios/find_path.py
+++ nebula_bench/scenarios/find_path.py
@@ -4,6 +4,6 @@ from nebula_bench.common.base import BaseScenario
 
 class FindShortestPath(BaseScenario):
     abstract = False
-    nGQL = "FIND SHORTEST PATH FROM {} TO {} OVER *"
+    nGQL = "FIND SHORTEST PATH FROM {} TO {} OVER * YIELD path as p"
     csv_path = "social_network/dynamic/person_knows_person.csv"
     csv_index = [0, 1]
