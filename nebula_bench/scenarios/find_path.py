7dd16570b12e34b7dd0a1a43eda4c084110d42bf
casualwinds <973566011@qq.com>
2023-02-28 11:53:53 +0800

    FindShortestNoVidPath (#81)
    * FindShortestNoVidPath
    
    * add the line

--- nebula_bench/scenarios/find_path.py
+++ nebula_bench/scenarios/find_path.py
@@ -2,8 +2,17 @@
 from nebula_bench.common.base import BaseScenario
 
 
-class FindShortestPath(BaseScenario):
-    abstract = False
+class BaseFindShortestPath(BaseScenario):
+    abstract = True
     nGQL = "FIND SHORTEST PATH FROM {0} TO {1} OVER * YIELD path as p"
     csv_path = "social_network/dynamic/person_knows_person.csv"
     rank = 100
+
+class FindShortestPath(BaseFindShortestPath):
+    abstract = False
+    nGQL = "FIND SHORTEST PATH FROM {0} TO {1} OVER * YIELD path as p"
+
+class FindShortestNoVidPath(BaseFindShortestPath):
+    abstract = False
+    nGQL = "FIND SHORTEST PATH FROM {0} TO -1 OVER * YIELD path as p"
+    
