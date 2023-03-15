6a039088bc29af3063b69f0b91978ed95c3ba15c
Sandy.lu <sandy.lu@vesoft.com>
2023-03-15 18:04:59 +0800

    add insertEdge Scenario (#83)
    * add insertEdge Scenario
    
    * add the line
    
    * add line

--- nebula_bench/scenarios/insert.py
+++ nebula_bench/scenarios/insert.py
@@ -2,7 +2,7 @@
 from nebula_bench.common.base import BaseScenario
 
 
-class BatchInsertScenario(BaseScenario):
+class BatchInsertVertexScenario(BaseScenario):
     is_insert_scenario = True
     nGQL = 'INSERT VERTEX Person(firstName, lastName, gender, birthday, creationDate, locationIP, browserUsed) VALUES '
     value = '{0}:("{1}", "{2}", "{3}", "{4}", datetime("{5}"), "{6}", "{7}")'
@@ -11,7 +11,7 @@ class BatchInsertScenario(BaseScenario):
     rank = 9999
 
 
-class InsertScenario(BaseScenario):
+class InsertVertexScenario(BaseScenario):
     is_insert_scenario = False
     nGQL = (
         'INSERT VERTEX Person(firstName, lastName, gender, birthday, creationDate, locationIP, browserUsed) VALUES '
@@ -20,3 +20,21 @@ class InsertScenario(BaseScenario):
     abstract = False
     csv_path = "social_network/dynamic/person.csv"
     rank = 9999
+
+class BatchInsertEdgeScenario(BaseScenario):
+    is_insert_scenario = True
+    nGQL = 'INSERT EDGE LIKES (creationDate) VALUES '
+    value = '{0}->{1}:(datetime("{2}"))'
+    abstract = False
+    csv_path = "social_network/dynamic/person_likes_comment.csv"
+    rank = 9999
+
+class InsertEdgeScenario(BaseScenario):
+    is_insert_scenario = False
+    nGQL = (
+        'INSERT EDGE LIKES (creationDate) VALUES {0}->{1}:(datetime("{2}"))'
+    )
+    abstract = False
+    csv_path = "social_network/dynamic/person_likes_comment.csv"
+    rank = 9999
+
